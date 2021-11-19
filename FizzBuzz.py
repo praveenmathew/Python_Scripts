57dd7bfabf67b33527b5566d5116d9674c373990
praveenmathew <praveenkuttinal@gmail.com>
2021-11-19 14:58:16 +0800

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,6 @@
+# Script for playing Fizz Buzz game, to learn more about conditionals. 
+# https://www.teclado.com/30-days-of-python/python-30-day-6-project
+
 for num in range(1,101):
     if (num % 3 == 0 and num % 5 == 0):
         print ("Fizz Buzz")
